42da9ea8869c97986e917e080cb46205900e9e3a
klyevatatt <klyevatatt@gmail.com>
2017-06-20 21:06:30 +0300

    Initial version of test project

---
+++ src/main/java/com/test/App.java
@@ -0,0 +1,10 @@
+package com.test;
+
+/**
+ * Created by Pisun on 20.06.2017.
+ */
+public class App {
+    public static void main(String[] arg){
+        System.out.println("Hello World");
+    }
+}
