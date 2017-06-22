3f6194d7de7757b57cf124a3bd0e09c7c6106e4a
klyevatatt <klyevatatt@gmail.com>
2017-06-22 10:06:50 +0300

    type data int and long

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -4,25 +4,20 @@ package com.test;
  * Created by Pisun on 20.06.2017.
  */
 public class App {
-    public static void main(String[] arg){
-       int x, y;
-       x=10;
-       y=20;
-       if(x<y)System.out.println("x<y");
-        x=x*2;
-        if(x==y) System.out.println("x=y");
-        x=x*2;
-        if (x>y) System.out.println("x>y");
-        if (x==y)System.out.println("You will not see this massage");
-
-        for (x=0;x<5;x++)
-        {System.out.println("Value x=" + x);}
-        System.out.println("Value x=" + x);
-        for (x=0;x<10;x++){
-            System.out.println("Value xx=" + x);
-            System.out.println("Value y=" + y);
-            y=y-2;
+    public static void main(String args[]){
+        int lightspeed;
+        int days;
+        int second;
+        int distance;
+        // приблизительная скорость света
+        lightspeed=186000;
+        days= 1000;
+        second=days*24*60*60;
+        distance=lightspeed*second;
+        System.out.print("For "+ days);
+        System.out.print(" days light went about ");
+        System.out.println(distance + " mile");
         }
-    }
+
 }
 
